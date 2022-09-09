2600382e136a25b0d3cafd5dbcf6911752778e7d
Silvana ALves <silvanarodriguesalvess@gmail.com>
2022-09-08 23:43:46 -0300

    V1.0

---
+++ media.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Olá pessoal");
+
+        var resultado = (6 + 8) / 2;
+        Console.Write("Media ");
+        Console.WriteLine(resultado);
+        //v1.0
+
+    }
+}
